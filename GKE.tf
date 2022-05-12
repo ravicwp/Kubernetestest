13bff2487d54d743956a929cc2715e649c953d8d
Pavan-Allenki <99476747+Pavan-Allenki@users.noreply.github.com>
2022-05-12 19:05:49 +0530

    Update GKE.tf

--- GKE.tf
+++ GKE.tf
@@ -2,12 +2,12 @@ resource "google_container_cluster" "example" {
   name               = var.name
   location           = var.location
   project            = data.google_project.project.name
-  enable_binary_authorization = true
-  enable_intranode_visibility = true
-  enable_shielded_nodes = true
+  enable_binary_authorization = false
+  enable_intranode_visibility = false
+  enable_shielded_nodes = false
   node_config {
     shielded_instance_config {
-      enable_integrity_monitoring = true
+      enable_integrity_monitoring = false
     }
      workload_metadata_config {
       node_metadata = "GKE_METADATA_SERVER"
@@ -18,8 +18,8 @@ resource "google_container_cluster" "example" {
   }
 
   private_cluster_config {
-       enable_private_nodes    = true
-       enable_private_endpoint = true
-       master_ipv4_cidr_block  = true
+       enable_private_nodes    = false
+       enable_private_endpoint = false
+       master_ipv4_cidr_block  = false
     }
 }
