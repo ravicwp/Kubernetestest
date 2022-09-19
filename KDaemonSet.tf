dbe73de7009f5deee35cded17b655ad548bed00a
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-09-19 13:31:44 +0530

    Update KDaemonSet.tf

--- KDaemonSet.tf
+++ KDaemonSet.tf
@@ -1,16 +1,16 @@
-resource "kubernetes_daemonset" "ravitest" {
+resource "kubernetes_daemonset" "ravitest1" {
   metadata {
     name      = "terraform-example"
     namespace = "default"
     labels = {
-      test = "ravitest"
+      test = "ravitest1"
     }
   }
 
   spec {
     selector {
       match_labels = {
-        test = "ravitest"
+        test = "ravitest1"
       }
     }
   }
